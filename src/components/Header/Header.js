b2f8dde03fc19f4cffc807565ef692e150692051
ankitingithub <as0617300@gmail.com>
2024-07-21 15:49:32 +0530

    Initial Commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,31 @@
-import "./Header.css"
+import { useState } from "react";
+import "./Header.css";
 
-function Header(){
-    return <div className="header">
-       <h1>my react store</h1> 
+function Header() {
+  const [showCart, setShowCart] = useState(false);
+
+  function openCart() {
+    setShowCart(true);
+  }
+
+  function closeCart() {
+    setShowCart(false);
+  }
+
+  return (
+    <div className="header">
+      <h1>my react store</h1>
+      <div>
+        <button className="yellow-button" onClick={openCart}>
+          Cart
+        </button>
+      </div>
+      {showCart && (
+        <div className="backdrop" onClick={closeCart}>
+          <div className="modal">Hello from cart </div>
+        </div>
+      )}
     </div>
+  );
 }
 export default Header;
